13364ea069fbb809df1b37173c4bcecb0239b945
galkinnikolay <galkin_nikolay@icloud.com>
2023-04-16 17:08:46 +0300

    задача 23-21

--- Zadacha21/Program.cs
+++ Zadacha21/Program.cs
@@ -7,3 +7,35 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 
 // A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+Console.Write("Введите число x1: ");
+string text_x1 = Console.ReadLine();
+int x1 = Convert.ToInt32(text_x1);
+
+Console.Write("Введите число y1: ");
+string text_y1 = Console.ReadLine();
+int y1 = Convert.ToInt32(text_y1);
+
+Console.Write("Введите число z1: ");
+string text_z1 = Console.ReadLine();
+int z1 = Convert.ToInt32(text_z1);
+
+Console.Write("Введите число x2: ");
+string text_x2 = Console.ReadLine();
+int x2 = Convert.ToInt32(text_x2);
+
+Console.Write("Введите число y2: ");
+string text_y2 = Console.ReadLine();
+int y2 = Convert.ToInt32(text_y2);
+
+Console.Write("Введите число z2: ");
+string text_z2 = Console.ReadLine();
+int z2 = Convert.ToInt32(text_z2);
+
+double x = Math.Pow((x2 - x1), 2);
+double y = Math.Pow((y2 - y1), 2);
+double z = Math.Pow((z2 - z1), 2);
+double AB = Math.Sqrt((x + y + z));
+Console.WriteLine(" ");
+Console.WriteLine($"Расстояние между точками А и В: {AB:f2}");
--- Zadacha23/Program.cs
+++ Zadacha23/Program.cs
@@ -7,3 +7,14 @@
 // 5 -> 1, 8, 27, 64, 125
 
 
+Console.Write("Введите число N: ");
+string text_N = Console.ReadLine();
+double N = Convert.ToInt32(text_N);
+double x = 1;
+double y = 1;
+while (x <= N)
+{
+    y = Math.Pow(x, 3);
+    Console.WriteLine(y);
+    x++;
+}
